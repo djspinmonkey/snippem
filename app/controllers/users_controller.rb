a82af5bd15199c54c62f8373fe56dfe673b3c328
John Hyland <github@djspinmonkey.com>
2009-01-15 18:46:37 -0500

    more prettification, corrected snippet order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = params[:id]
-    @snippets = Snippet.find_all_by_user(@user)
+    @snippets = Snippet.find_all_by_user(@user, :order => 'created_at DESC')
 
     respond_to do |format|
       format.html # show.html.erb
