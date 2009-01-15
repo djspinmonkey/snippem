e30708f1591f0559ef7d5a0428f6fd8e76fe7261
John Hyland <github@djspinmonkey.com>
2009-01-15 18:40:05 -0500

    prettify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    @snippets = Snippet.find_all_by_user(params[:id])
+    @user = params[:id]
+    @snippets = Snippet.find_all_by_user(@user)
 
     respond_to do |format|
       format.html # show.html.erb
