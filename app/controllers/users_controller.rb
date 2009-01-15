5ede1c8ddd44fa94aea4b0f70f231c7619f50bed
John Hyland <github@djspinmonkey.com>
2009-01-15 18:55:58 -0500

    more prettification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = Snippet.find(:all).collect { |s| s.user }.sort.uniq  # suboptimal!
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+
   def show
     @user = params[:id]
     @snippets = Snippet.find_all_by_user(@user, :order => 'created_at DESC')
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @snippet }
+      format.xml  { render :xml => @snippets }
     end
   end
 
