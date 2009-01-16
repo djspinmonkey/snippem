8cbd8af920d6c5194d59cb45a00aafe395fc2315
John Hyland <github@djspinmonkey.com>
2009-01-15 19:08:19 -0500

    hackety hack

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '70dae56a5805d05e169b9aca08fc7d70'
+  #protect_from_forgery # :secret => '70dae56a5805d05e169b9aca08fc7d70'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
